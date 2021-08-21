db0b4285c97e9293e8e38adf175f39976a03ca6b
Emiliano <emilianop96@hotmail.com>
2021-08-21 20:08:15 -0300

    add models using sequelize-cli

---
+++ models/vehicleproperty.js
@@ -0,0 +1,25 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class VehicleProperty extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      VehicleProperty.belongsToMany(models.Vehicle, { through: 'PropertyValue' });
+      VehicleProperty.belongsTo(models.Category);
+    }
+  }
+  VehicleProperty.init(
+    {
+      name: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: 'VehicleProperty',
+    }
+  );
+  return VehicleProperty;
+};
