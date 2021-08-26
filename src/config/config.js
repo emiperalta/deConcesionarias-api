1927ecc55a109264abdb067692e14e7c01a09a53
Emiliano <emilianop96@hotmail.com>
2021-08-26 01:55:53 -0300

    configure ssl for db

--- src/config/config.js
+++ src/config/config.js
@@ -18,5 +18,10 @@ module.exports = {
   production: {
     use_env_variable: process.env.DATABASE_URL,
     dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    },
   },
 };
