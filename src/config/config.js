247ba25596906f84c7a4348eaa180b27d1420b2a
Emiliano <emilianop96@hotmail.com>
2021-08-26 01:10:34 -0300

    change production config

--- src/config/config.js
+++ src/config/config.js
@@ -16,10 +16,7 @@ module.exports = {
     dialect: 'postgres',
   },
   production: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    host: process.env.DB_HOST,
+    connectionString: process.env.DATABASE_URL,
     dialect: 'postgres',
   },
 };
